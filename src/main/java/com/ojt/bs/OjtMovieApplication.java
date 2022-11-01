a1e5e06986800088849d543267c800444f5c2faf
winmon <winmon@localhost>
2022-11-01 22:31:20 +0630

    setup project to develope

---
+++ src/main/java/com/ojt/bs/OjtMovieApplication.java
@@ -0,0 +1,13 @@
+package com.ojt.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjtMovieApplication.class, args);
+	}
+
+}
